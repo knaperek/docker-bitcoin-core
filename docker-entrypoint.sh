808ac8334d74797235ad15a1aaa2f8cfbd535869
Jozef Knaperek <jknaperek@gmail.com>
2018-08-12 21:25:45 +0200

    Simplify entrypoint - no need to create data folder dynamically

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,27 +1,13 @@
 #!/bin/dash
 set -e
 
-if [ "$1" = "bitcoin-cli" -o "$1" = "bitcoind" ]; then
-	mkdir -p "$BITCOIN_DATA"
-
-	if [ ! -s "$BITCOIN_DATA/bitcoin.conf" ]; then
-		cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
-		printtoconsole=1
-		rpcallowip=::/0
-		rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
-		rpcuser=${BITCOIN_RPC_USER:-bitcoin}
-		EOF
-		chown bitcoin:bitcoin "$BITCOIN_DATA/bitcoin.conf"
-	fi
-
-	# ensure correct ownership and linking of data directory
-	# we do not update group ownership here, in case users want to mount
-	# a host directory and still retain access to it
-	chown -R bitcoin "$BITCOIN_DATA"
-	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.bitcoin
-	chown -h bitcoin:bitcoin /home/bitcoin/.bitcoin
-
-	exec gosu bitcoin "$@"
+if [ ! -s "$BITCOIN_DATA/bitcoin.conf" ]; then
+	cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
+	printtoconsole=1
+	rpcallowip=::/0
+	rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
+	rpcuser=${BITCOIN_RPC_USER:-bitcoin}
+	EOF
 fi
 
 exec "$@"
